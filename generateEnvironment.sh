bdb0e10c1ab9e8f77c190aaa8f8419cae646e47b
Antonio Ángel Guirola Vicente <antonioguirola@gmail.com>
2014-01-10 11:17:47 +0100

    Primera MV creada

---
+++ generateEnvironment.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Instalación de los paquetes necesarios
+sudo apt-get install -y python-virtualenv
+sudo apt-get install -y git
+sudo apt-get install -y python-dev
+sudo apt-get install -y libxml2-dev libxslt-dev
+
+# Creación del entorno virtual Python
+cd ~
+mkdir env
+cd env
+virtualenv --distribute env1
+cd env1/
+source bin/activate
+
+# Instalación de los paquetes Python necesarios
+# dentro del entorno virtual
+pip install web.py
+pip install mako
+pip install pymongo
+pip install lxml
+pip install tweepy
+
+# Recuperamos la aplicación del repositorio privado
+git clone https://antonioguirola@bitbucket.org/antonioguirola/appdai.git
+cd appdai
+
+# Ejecutamos el servidor web de web.py en el puerto 80
+python code.py 80
